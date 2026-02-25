a885a2f8a7b90a1f4cced530b6f6fa47a2cf7940
konrepo <khmerboxtv@gmail.com>
2026-02-24 21:57:03 -0500

    Fix meta handler return format

--- index.js
+++ index.js
@@ -62,10 +62,49 @@ builder.defineCatalogHandler(async ({ id }) => {
     }
 });
 
-builder.defineMetaHandler(async (args) => {
-    console.log("META ARGS:", JSON.stringify(args));
+builder.defineMetaHandler(async ({ type, id }) => {
+    if (type !== "series") return { meta: null };
 
-    return { metas: [] };
+    try {
+        const { data } = await axios.get(id, {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+            }
+        });
+
+        const $ = cheerio.load(data);
+
+        const episodes = [];
+        let epNumber = 1;
+
+        $("table#latest-videos a[href], div.col-xs-6.col-sm-6.col-md-3 a[href]")
+            .each((i, el) => {
+                const link = $(el).attr("href");
+                if (link) {
+                    episodes.push({
+                        id: link,
+                        season: 1,
+                        episode: epNumber,
+                        name: `Episode ${String(epNumber).padStart(2, "0")}`
+                    });
+                    epNumber++;
+                }
+            });
+
+        return {
+            meta: {
+                id,
+                type: "series",
+                name: id.split("/").filter(Boolean).pop().replace(/-/g, " "),
+                episodes
+            }
+        };
+
+    } catch (err) {
+        console.error("Meta error:", err.message);
+        return { meta: null };
+    }
 });
 
 builder.defineStreamHandler(async () => {
