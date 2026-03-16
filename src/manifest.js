bf6ab5a2be18d8dce4502b5434953cbbf255810d
konrepo <khmerboxtv@gmail.com>
2026-03-15 22:38:39 -0400

    Improve code and misc cleaning

--- src/manifest.js
+++ src/manifest.js
@@ -1,41 +1,32 @@
+const TYPE = "series";
+const EXTRA = ["search", "skip"];
+
+const sites = [
+  { id: "vip", name: "Phumikhmer" },
+  { id: "sunday", name: "SundayDrama" },
+  { id: "idrama", name: "iDramaHD" },
+  { id: "khmerave", name: "KhmerAve" },
+  { id: "merlkon", name: "Merlkon" },
+];
+
 module.exports = {
   id: "community.khmer.dubbed",
-  version: "1.1.1",
+  version: "2.0.0",
   name: "KhmerDub",
   description: "Stream Asian dramas dubbed in Khmer | Dev: TheDevilz.",
   logo: "https://avatars.githubusercontent.com/u/32822347?v=4",
+
   resources: ["catalog", "meta", "stream"],
-  types: ["series"],
-  catalogs: [
-    {
-      type: "series",
-      id: "vip",
-      name: "Phumikhmer",
-      extraSupported: ["search", "skip"],
-    },
-    {
-      type: "series",
-      id: "sunday",
-      name: "SundayDrama",
-      extraSupported: ["search", "skip"],
-    },	
-    {
-      type: "series",
-      id: "idrama",
-      name: "iDramaHD",
-      extraSupported: ["search", "skip"],
-    },
-    {
-      type: "series",
-      id: "khmerave",
-      name: "KhmerAve",
-      extraSupported: ["search", "skip"],
-    },
-    {
-      type: "series",
-      id: "merlkon",
-      name: "Merlkon",
-      extraSupported: ["search", "skip"],
-    },
-  ],
+  types: [TYPE],
+
+  catalogs: sites.map(site => ({
+    type: TYPE,
+    id: site.id,
+    name: site.name,
+    extraSupported: EXTRA
+  })),
+
+  behaviorHints: {
+    configurable: false
+  }
 };
